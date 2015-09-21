c28ab06dc03bfc298d7013272c2857b00ee13b19
Nyuno <nyunohyuno@gmail.com>
2015-09-21 02:28:35 +0000

    add layout erb file and homes create action

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -4,4 +4,28 @@ class HomesController < ApplicationController
       @a = Instagram.client(access_token: session[:token]).user_recent_media
     end
   end
+
+  def create
+    require 'erb'
+    erb_file = File.join(Rails.root + 'app/templates/layout.html.erb')
+    html_file = File.basename(erb_file, '.erb') #=>"page.html"
+    erb_str = File.read(erb_file)
+
+    @name = "Nyuno"
+    renderer = ERB.new(erb_str)
+    result = renderer.result()
+
+    system "mkdir #{@name}"
+    File.open(@name + '/' + html_file, 'w') do |f|
+        f.write(result)
+    end
+    ####### end generate html file
+    #
+    ####### TODO
+
+    #system "aws s3 cp #{@name} s3://rehash-dev.com/#{@name} --recursive"
+    #system "rm -rf #{@name}"
+
+    redirect_to root_path
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root 'homes#index'
+  resources :homes
 
   get '/auth/:provider/callback', to: "auth_callbacks#create"
 end
