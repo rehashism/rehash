4d8e942b5a9368a07fc77ed89019d92c6b2f7089
eungjinlee <oiojin831@gmail.com>
2015-09-22 10:41:04 +0000

    automate aws works

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -25,7 +25,21 @@ class HomesController < ApplicationController
 
     #system "aws s3 cp #{@name} s3://rehash-dev.com/#{@name} --recursive"
     #system "rm -rf #{@name}"
+    #
+    generate_s3
 
     redirect_to root_path
+  end
+
+  private
+
+  def generate_s3
+    system "aws s3 mb s3://#{@name}.rehashism.com"
+    system "aws s3 rm s3://#{@name}.rehashism.com --recursive"
+    system %Q[aws s3 sync #{@name} s3://#{@name}.rehashism.com --acl public-read --cache-control "public, max-age=86400"]
+    system "aws s3 website s3://#{@name}.rehashism.com --index-document layout.html"
+    system "rm -rf #{@name}"
+
+    system %Q[aws route53 change-resource-record-sets --hosted-zone-id ZXXTN8ME7HY7P --change-batch file://app/templates/route53.json]
   end
 end
