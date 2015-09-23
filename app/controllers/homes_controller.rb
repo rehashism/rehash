63b2b4470944bd247d1d2150715624fc9a2806fd
eungjinlee <oiojin831@gmail.com>
2015-09-23 11:03:13 +0000

    Add autogenerating pages

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -13,19 +13,27 @@ class HomesController < ApplicationController
 
     @name = current_user.identities[0].nickname
     renderer = ERB.new(erb_str)
-    result = renderer.result()
+    result = renderer.result(binding)
+    #need to figure out if result can pass @name first then try json
 
     system "mkdir #{@name}"
     File.open(@name + '/' + html_file, 'w') do |f|
         f.write(result)
     end
-    ####### end generate html file
-    #
-    ####### TODO
 
-    #system "aws s3 cp #{@name} s3://rehash-dev.com/#{@name} --recursive"
-    #system "rm -rf #{@name}"
-    #
+    ####### end generate html file
+    require 'json'
+    json_erb_file = File.join(Rails.root + 'app/templates/route53.json.erb')
+    json_file = File.basename(json_erb_file, '.erb')
+    json_erb_str = File.read(json_erb_file)
+
+    renderer = ERB.new(json_erb_str)
+    json_result = renderer.result(binding)
+
+    File.open(@name + '/' + json_file, 'w') do |f|
+      f.write(json_result)
+    end
+
     generate_s3
 
     redirect_to root_path
@@ -38,8 +46,9 @@ class HomesController < ApplicationController
     system "aws s3 rm s3://#{@name}.rehashism.com --recursive"
     system %Q[aws s3 sync #{@name} s3://#{@name}.rehashism.com --acl public-read --cache-control "public, max-age=86400"]
     system "aws s3 website s3://#{@name}.rehashism.com --index-document layout.html"
-    system "rm -rf #{@name}"
 
-    system %Q[aws route53 change-resource-record-sets --hosted-zone-id ZXXTN8ME7HY7P --change-batch file://app/templates/route53.json]
+    system %Q[aws route53 change-resource-record-sets --hosted-zone-id ZXXTN8ME7HY7P --change-batch file://#{@name}/route53.json]
+
+    system "rm -rf #{@name}"
   end
 end
