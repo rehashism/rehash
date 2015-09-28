95b70a8df25bb469eea8112cbf5297841a176c11
Nyuno <nyunohyuno@gmail.com>
2015-09-28 01:10:32 +0000

    create page_tasker.rb

--- app/models/page_generator.rb
+++ app/models/page_generator.rb
@@ -1,5 +1,3 @@
-require 'erb'
-require 'json'
 class PageGenerator
   attr_reader :page_name
 
@@ -8,38 +6,33 @@ class PageGenerator
     @page_name = @current_user.store.name
   end
 
-  def generate_page
-    erb_file = File.join(Rails.root + 'app/templates/layout.html.erb')
-    html_file = File.basename(erb_file, '.erb') #=>"page.html"
-    erb_str = File.read(erb_file)
-
-    renderer = ERB.new(erb_str)
-    result = renderer.result(binding)
-
+  def make_directory
     system "mkdir #{@page_name}"
-    File.open(@page_name + '/' + html_file, 'w') do |f|
-        f.write(result)
-    end
+  end
+
+  def generate_page
+    write_file('layout.html')
   end
 
   def generate_json
-    json_erb_file = File.join(Rails.root + 'app/templates/route53.json.erb')
-    json_file = File.basename(json_erb_file, '.erb')
-    json_erb_str = File.read(json_erb_file)
-
-    renderer = ERB.new(json_erb_str)
-    json_result = renderer.result(binding)
-
-    File.open(@page_name + '/' + json_file, 'w') do |f|
-      f.write(json_result)
-    end
-
-    generate_s3
+    write_file('route53.json')
   end
 
-  def file_open(file_name)
-    erb_file = File.join(Rails.root + "app/templates/#{file_name}")
+  def generate_files
+    generate_page
+    generate_json
+  end
 
+  private
+  def write_file(file_name)
+    erb_file = File.join(Rails.root + "app/templates/#{file_name}.erb")
+    basic_file = File.basename(erb_file, '.erb')
+    erb_str = File.read(erb_file)
+    
+    renderer = ERB.new(erb_str)
+    result = renderer.result(binding)
+
+    File.open(@page_name + '/' + basic_file, 'w') { |f| f.write(result)  }
   end
 
 end
---
+++ app/models/page_tasker.rb
@@ -0,0 +1,37 @@
+class PageTasker
+  def initialize(access_token, current_user)
+    @generator = PageGenerator.new(current_user)
+    @parser = InstagramParser.new(access_token, current_user)
+  end
+
+  def first_generate_rehash
+    @generator.make_directory
+    generate_rehash
+  end
+
+  def refresh_rehash
+    @parser.delete_menus
+    generate_rehash
+  end
+
+  private
+  attr_reader :generator, :parser
+
+  def generate_rehash
+    @parser.create_menus
+    @generator.generate_files
+    generate_s3(@generator.page_name)
+  end
+
+  def generate_s3(page_name)
+    system "aws s3 mb s3://#{page_name}.rehashism.com"
+    system "aws s3 rm s3://#{page_name}.rehashism.com --recursive"
+    system %Q[aws s3 sync #{page_name} s3://#{page_name}.rehashism.com --acl public-read --cache-control "public, max-age=86400"]
+    system "aws s3 website s3://#{page_name}.rehashism.com --index-document layout.html"
+
+    system %Q[aws route53 change-resource-record-sets --hosted-zone-id ZXXTN8ME7HY7P --change-batch file://#{page_name}/route53.json]
+
+    system "rm -rf #{page_name}"
+
+  end
+end
