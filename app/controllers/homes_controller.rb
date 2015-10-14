9525e7e19aef91fd79721c3128b509776dbac6cb
eungjinlee <oiojin831@gmail.com>
2015-10-14 08:33:54 +0000

    Redirct to generated page

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -7,7 +7,7 @@ class HomesController < ApplicationController
 
   def create
     PageTasker.new(session[:token], current_user).generate_or_refresh_rehash(params[:type])
-    redirect_to root_path
+    redirect_to "http://#{current_user.store.name}.rehashism.com"
   end
 
 end
